ce627ec987121a87847985963ecfa3125bf92b46
Ace Gamal <ahmed.elhusseini27@gmail.com>
2024-05-06 12:31:18 +0300

    Update server.scala

--- src/main/scala/server.scala
+++ src/main/scala/server.scala
@@ -4,14 +4,14 @@ import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.middleware.*
 import org.http4s.dsl.*
 import org.http4s.*
-import API.API
+import API._
 import com.comcast.ip4s.{ipv4, port}
 
 
 object server extends IOApp.Simple{
 
   val corsService = CORS.policy
-    .withAllowOriginAll.apply(API.service) // Allow all origins (adjust as needed)
+    .withAllowOriginAll.apply(service) // Allow all origins (adjust as needed)
 
   val serverResource = EmberServerBuilder
     .default[IO]
